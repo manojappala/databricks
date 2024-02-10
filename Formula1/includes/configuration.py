2a60081fda6a9d6f609ea53960d283a133a85192
manojappala <manojappala>
2024-02-10 00:10:58 +0000

    updated runbook functions

---
+++ Formula1/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/testdljan2024/raw/formula1/'
+processed_folder_path = '/mnt/testdljan2024/raw/formula1/processed'
+presentation_folder_path = '/mnt/testdljan2024/raw/formula1/presentation'
+
+# COMMAND ----------
+
+
---
+++ Formula1/includes/ingest circuit file.py
@@ -0,0 +1,96 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ingest circuit.csv files
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %run "./configuration"
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %run "./common_functions"
+
+# COMMAND ----------
+
+raw_folder_path
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[
+    StructField("circuitId", IntegerType(), False),
+    StructField("circuitRef", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("location", StringType(), True),
+    StructField("country", StringType(), True),
+    StructField("lat", DoubleType(), True),
+    StructField("lng", DoubleType(), True),
+    StructField("alt", IntegerType(), True),
+    StructField("url", IntegerType(), True),
+    ])
+
+# COMMAND ----------
+
+circuits_df = spark.read.option("header", True).schema(circuits_schema).csv(f"{raw_folder_path}/circuits.csv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC select the columns we want
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select("circuitId", "circuitRef", "name", "location", "country", "lat", "lng", "alt")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select(col("circuitId"), col("circuitRef"), col("name"), col("location"), col("country").alias("race_country"), col("lat").alias("latitude"), col("lng").alias("longitude"), col("alt").alias("altitude"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC we can aslo change the colums name with data.frame.withcolumnrenames(existing,new)
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId" , "circuit_id") \
+    .withColumnRenamed("circuitRef", "circuit_ref") \
+    .withColumnRenamed("lat", "latitude") \
+    .withColumnRenamed("lng", "longitude") \
+    .withColumnRenamed("alt", "altitude") 
+
+# COMMAND ----------
+
+circuits_final_df = add_ingestion_date(circuits_renamed_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, lit
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp()) \
+    .withColumn("env", lit("Production"))
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Dataframe write
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").parquet(f"{processed_folder_path}/circuits")
+
+# COMMAND ----------
+
+
